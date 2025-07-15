abe484fa963289d240823eca4601d8f60389cdaf
Mark Burggraf <markb@mantisbible.com>
2025-07-15 12:15:58 -0700

    Fix: Disable react-native-screens to prevent RNSScreenNavigationContainer error

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,6 +4,10 @@ import { useAuthStore } from '../stores/authStore';
 import AuthNavigator from './AuthNavigator';
 import MainNavigator from './MainNavigator';
 
+// Disable react-native-screens to prevent crashes in TestFlight
+import { enableScreens } from 'react-native-screens';
+enableScreens(false);
+
 export default function AppNavigator() {
   const { user } = useAuthStore();
 
